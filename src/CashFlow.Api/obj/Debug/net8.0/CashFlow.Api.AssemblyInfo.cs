530227efbc05724ebf7bfd89f97ad63fb4277e04
Fernando Futila <fernando.futila@gmail.com>
2024-09-06 09:14:24 -0300

    feat: validating the language received

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd41b52c053c63799294bd43dfa38c381ee3fa9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1ac1a1e5e0b0a32bb207d2b7a6a8f71b90e3b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
